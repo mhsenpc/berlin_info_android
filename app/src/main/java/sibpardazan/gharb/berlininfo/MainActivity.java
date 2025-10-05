3da2e51df8512ed6353603be13825f23cc4c11b5
Mohsen Shamohammadi <mohsen.shamohammadi@atolls.com>
2025-10-06 00:24:43 +0200

    more places

--- app/src/main/java/sibpardazan/gharb/berlininfo/MainActivity.java
+++ app/src/main/java/sibpardazan/gharb/berlininfo/MainActivity.java
@@ -36,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
         MaterialCardView cardCheckpointCharlie = findViewById(R.id.card_checkpoint_charlie);
         MaterialCardView cardBerlinZoo = findViewById(R.id.card_berlin_zoo);
 
+        MaterialCardView cardTvTower = findViewById(R.id.card_tv_tower);
+        MaterialCardView cardHolocaustMemorial = findViewById(R.id.card_holocaust_memorial);
+        MaterialCardView cardUnterDenLinden = findViewById(R.id.card_unter_den_linden);
+        MaterialCardView cardKaiserWilhelmChurch = findViewById(R.id.card_kaiser_wilhelm_church);
+        MaterialCardView cardGropiusBau = findViewById(R.id.card_gropius_bau);
+        MaterialCardView cardTempelhofAirport = findViewById(R.id.card_tempelhof_airport);
+        MaterialCardView cardHackescherMarkt = findViewById(R.id.card_hackescher_markt);
+        MaterialCardView cardSpandauCitadel = findViewById(R.id.card_spandau_citadel);
+        MaterialCardView cardTreptowerPark = findViewById(R.id.card_treptower_park);
+        MaterialCardView cardEastSideGallery = findViewById(R.id.card_east_side_gallery);
+
         cardBrandenburgGate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,6 +114,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, BerlinZooActivity.class));
+            }
+        });
+
+        cardTvTower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TvTowerActivity.class));
+            }
+        });
+
+        cardHolocaustMemorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HolocaustMemorialActivity.class));
+            }
+        });
+
+        cardUnterDenLinden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UnterDenLindenActivity.class));
+            }
+        });
+
+        cardKaiserWilhelmChurch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, KaiserWilhelmChurchActivity.class));
+            }
+        });
+
+        cardGropiusBau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GropiusBauActivity.class));
+            }
+        });
+
+        cardTempelhofAirport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TempelhofAirportActivity.class));
+            }
+        });
+
+        cardHackescherMarkt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HackescherMarktActivity.class));
+            }
+        });
+
+        cardSpandauCitadel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpandauCitadelActivity.class));
+            }
+        });
+
+        cardTreptowerPark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TreptowerParkActivity.class));
+            }
+        });
+
+        cardEastSideGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EastSideGalleryActivity.class));
             }
         });
     }
---
+++ app/src/main/java/sibpardazan/gharb/berlininfo/UnterDenLindenActivity.java
@@ -0,0 +1,36 @@
+package sibpardazan.gharb.berlininfo;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class UnterDenLindenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unter_den_linden);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.unter_den_linden);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
